e9b8068ed862eda035fe2777f1797d2426cdd465
Mark Stephens <mark.stephens@ft.com>
2013-05-20 21:08:11 +0100

    Frontend

--- webapp/src/main/webapp/js/homepage.js
+++ webapp/src/main/webapp/js/homepage.js
@@ -4,7 +4,7 @@
         $(document).on('dynamitePopularContentReady', function(event, data) {
             var homepage_top5 = $(document.createElement('div'));
             homepage_top5.addClass('dynamite article package');
-            homepage_top5.append('<h1><a href="/teamDynamite.html">People like you, also read</a></h1>');
+            homepage_top5.append('<h1><a href="teamDynamite.html">People like you, also read</a></h1>');
             homepage_top5.append(renderTop5(data));
             $('.master-row.contentSection').prepend(homepage_top5);
         });
@@ -14,12 +14,14 @@
         var i, html = [];
 
         for (i = 0; i < dynamite.areas.length; i++) {
-            if (data[dynamite.areas[i].dataName].length > 0) {
-                html.push('<div class="article first image package">');
-                html.push('<h2>');
-                html.push(dynamite.areas[i].title.replace('**name**', data[dynamite.areas[i].areaName]));
-                html.push('</h2><ul>');
-                html.push(renderArea(data[dynamite.areas[i].dataName]));
+            if (data.hasOwnProperty(dynamite.areas[i].dataName)) {
+                if (data[dynamite.areas[i].dataName].length > 0) {
+                    html.push('<div class="article first image package">');
+                    html.push('<h2>');
+                    html.push(dynamite.areas[i].title.replace('**name**', data[dynamite.areas[i].areaName]));
+                    html.push('</h2><ul>');
+                    html.push(renderArea(data[dynamite.areas[i].dataName]));
+                }
             }
         }
 
