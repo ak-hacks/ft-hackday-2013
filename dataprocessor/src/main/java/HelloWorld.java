c7789173675cad4ceed1d0a6ba398ba37c5ca111
Anurag Kapur <anuragkapur@gmail.com>
2013-05-20 14:10:10 +0100

    Hello world for dataprocessors component

---
+++ dataprocessor/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * User: anuragkapur
+ * Date: 20/05/2013 14:09
+ */
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!!");
+    }
+}
